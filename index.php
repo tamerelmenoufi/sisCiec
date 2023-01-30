77afbc9729a6f9e3cc6f039bc170d22b347d4c23
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-30 08:44:40 -0400

    Atualizações

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 				<a href='./sistema/' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">DJALMA BATISTA</a>
 			</td>
 
-			<td width="33%" height="100" align="center" valign="middle">
+			<!-- <td width="33%" height="100" align="center" valign="middle">
 				<a href='./saojose/' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">SÃO JOSÉ</a>
-			</td>
+			</td> -->
 
 			<td width="33%" height="100" align="center" valign="middle">
 				<a href='./nacoes/' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">PARQUE DAS NAÇÕES</a>
