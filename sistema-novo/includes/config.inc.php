529245fa555e78ab2a10af2f0075e0b2fd59ac50
livia128924 <livia128924@gmail.com>
2023-02-03 19:09:10 -0300

    atualizando

--- sistema-novo/includes/config.inc.php
+++ sistema-novo/includes/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$servidor_local = '/docker/wm/docker/www/sistema'; //local absoluto no servidor
-$servidor_url = 'http://'.$_SERVER['SERVER_NAME'].':8087/sistema'; //endere?o url do sistema
+$servidor_local = '/docker/wm/docker/www/sistema-novo'; //local absoluto no servidor
+$servidor_url = 'http://'.$_SERVER['SERVER_NAME'].':8087/sistema-novo'; //endere?o url do sistema
 
 ?>
