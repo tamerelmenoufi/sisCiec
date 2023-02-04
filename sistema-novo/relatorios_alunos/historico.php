93b6d32b41e753f2b3c9dbfe152d6520f9fcd854
livia128924 <livia128924@gmail.com>
2023-02-03 23:32:55 -0300

    atualizando

--- sistema-novo/relatorios_alunos/historico.php
+++ sistema-novo/relatorios_alunos/historico.php
@@ -339,11 +339,11 @@ Sem pend&ecirc;ncia de disciplinas no curso <?=$_CursoNome[$val]?>
       <td align="center" class="arial10">
       Djalma Batista<br>
       Milhomem Center, Av. Djalma Batista, nº 98A<br>
-             3023-1242 / 3346-0191 / 3236-4048
+             3023-1242 / 3346-0191 / 99303-9416
         </td>
       <td align="center" class="arial10">
       Shopping São José<br>
-      2º Piso em frente ao DETRAN<br>
+      2º Piso em frente a Marisa<br>
             3342-3327
         </td>
       <td align="center" class="arial10">
