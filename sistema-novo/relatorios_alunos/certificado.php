67b85460e4ac70c4e4493a6eec49dfa02ac11b27
livia128924 <livia128924@gmail.com>
2023-02-14 16:17:27 -0300

    atualizando

--- sistema-novo/relatorios_alunos/certificado.php
+++ sistema-novo/relatorios_alunos/certificado.php
@@ -650,11 +650,11 @@ Observações
        </div>
        
        
-    	<div style="margin-top:5px;text-align:right;padding:4px;margin-bottom:15px"><?=data()?>.</div>
+    	<div style="margin-top:5px;text-align:right;padding:4px;margin: bottom 15px;"><?=data()?>.</div>
    
       <table class="">
     <tr>
-    
+    <td width="380"  ></th>
     <td width="350" style="border-top:1px #000 solid;text-align:center">Secretário(a)  
     </td>
    
