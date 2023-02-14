27376be20d48ab20f18c11b2aec5c32c1cd227f7
livia128924 <livia128924@gmail.com>
2023-02-14 11:31:43 -0300

    atualizando

--- sistema-novo/relatorios_alunos/certificado.php
+++ sistema-novo/relatorios_alunos/certificado.php
@@ -583,13 +583,13 @@ white-space:nowrap;
 
       <tr>
         <td width="150" height="25" class="times12" style="border-bottom: 1px solid #000;border-top: 1px solid #000;font-weight:bold">Total de Pontos  </td>
-                <td width="104" class="times12" style="border: solid 1px #000;font-weight:bold"><div align="center"  class="times12">86,5</div></td>
+                <td width="104" class="times12" style="border: solid 1px #000;font-weight:bold;border-bottom:0px"><div align="center"  class="times12">86,5</div></td>
         <td width="413" class="times12" align="center" style="border: solid 1px #000;border-left:0px;border-bottom:0px"></td>
       
       </tr>
       <tr>
         <td width="150" height="25" class="times12" style="font-weight:bold">Coeficiente </td>
-                <td width="104" class="times12" style="border: solid 1px #000;font-weight:bold"><div align="center" class="times12">7,20</div></td>
+                <td width="104" class="times12" style="border: solid 1px #000;font-weight:bold;border-bottom:0px"><div align="center" class="times12">7,20</div></td>
         <td width="413" class="times12" align="center" style="border: solid 1px #000;border-left:0px;border-bottom:0px"></td>
       
       </tr>
