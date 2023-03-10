3c6e07578c437fae17a2891115172e3f384a91c8
livia128924 <livia128924@gmail.com>
2023-03-10 12:00:51 -0300

    atualizando

--- sistema-novo/relatorios_alunos/atestado_eliminacao.php
+++ sistema-novo/relatorios_alunos/atestado_eliminacao.php
@@ -24,12 +24,12 @@ if($_POST[observacao]){
 	$dados = mysql_fetch_object($result);
 
   if($dados->rg){
-      $linha1 = "RG ".$dados->rg;
+      $linha1 = "RG ".$dados->rg." ".$dados->rg_orgao;
       $linha2 = "ORG&Atilde;O/UF: ".$dados->rg_orgao;
       $linha3 = "CIDADE: ".$dados->cidade;
       $linha4 = "NASCIMENTO: ".data_formata($dados->data_nascimento);
   }elseif($dados->rne){
-      $linha1 = "RNE: ".$dados->rne;
+      $linha1 = "RNE ".$dados->rne;
       $linha2 = "Nacionalidade: ".$dados->nacionalidade;
       $linha3 = false;
       $linha4 = false;
