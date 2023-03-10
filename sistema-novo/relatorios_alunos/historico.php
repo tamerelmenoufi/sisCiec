9558a1754bf3f08464979a11e7a6722a84b34904
livia128924 <livia128924@gmail.com>
2023-03-10 13:14:48 -0300

    atualizando

--- sistema-novo/relatorios_alunos/historico.php
+++ sistema-novo/relatorios_alunos/historico.php
@@ -311,10 +311,10 @@ Disciplinas pendentes curso <?=$_CursoNome[$val]?> (
 <?php
 }else{
 ?>
-<font style="text-align:center" color="#006600"><b>
+<p style="text-align:center" color="#006600"><b>
 Sem pend&ecirc;ncia de disciplinas no curso <?=$_CursoNome[$val]?> 
 
-</b></font><font color="#006600"><b>
+</b></p><font color="#006600"><b>
 <?php
 }
 ?>
