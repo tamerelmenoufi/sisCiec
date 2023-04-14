1ace2e2587d32d4f753ae813174c2e56f5ec4a43
livia128924 <livia128924@gmail.com>
2023-04-14 20:42:25 -0300

    atualizando

--- sistema-novo/includes/topoDoc.php
+++ sistema-novo/includes/topoDoc.php
@@ -1,6 +1,6 @@
 <table width="800" border="0" align="center" cellpadding="10" cellspacing="0">
   <tr>
-    <td width="150" align="center" valign="middle"><img src="../img/logo_ciec.gif" width="150" /></td>
+    <td width="150" align="center" valign="middle" style="padding:0px"><img src="../img/logo_ciec.gif" width="150" /></td>
     <td align="left" valign="middle"><strong><?=$conf[unidade]?></strong></td>
   </tr>
 </table>
