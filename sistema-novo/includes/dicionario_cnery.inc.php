f9c78196b0eb94c62c704c6f07b61f0377347183
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-19 22:41:05 +0200

    Atualizações

--- sistema-novo/includes/dicionario_cnery.inc.php
+++ sistema-novo/includes/dicionario_cnery.inc.php
@@ -5,6 +5,6 @@
         'resolucao' => 'Lei Federal n. 9394/96, de 20 de dezembro de 1996.<br>',
         'r' => 'Lei Federal n. 9394/96, de 20 de dezembro de 1996.',
         'endereço' => 'Rua fulano de tal',
-        'rodape_endereco1' => 'Djalma Batista<br>Ed. Milhomem Center, Av. Djalma Batista, n° 98A<br>3023-1242 / 3346-0191 / 99303-9416',
+        'rodape_endereco1' => 'Djalma Batista Alterado<br>Ed. Milhomem Center, Av. Djalma Batista, n° 98A<br>3023-1242 / 3346-0191 / 99303-9416',
 
     ];
