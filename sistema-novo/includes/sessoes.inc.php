7a6c33192b46f118ec9cfadf4656cf515925fda3
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-19 16:13:01 +0200

    Atualizações

--- sistema-novo/includes/sessoes.inc.php
+++ sistema-novo/includes/sessoes.inc.php
@@ -31,6 +31,7 @@ if($_GET['escola']){
 
 if($_COOKIE['confUnidade']){
 	include("../includes/dicionario_{$_COOKIE['confUnidade']}.inc.php");
+	echo "ESTOU NA UNIDADE: ".$_COOKIE['confUnidade'];
 }
 /////////////////////////////////////////////////////////////////
 
