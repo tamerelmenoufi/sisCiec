66fd44ea101034823f93fdb143ad5b3db43ddd40
livia128924 <livia128924@gmail.com>
2023-04-19 18:20:51 -0300

    atualizando

--- sistema-novo/includes/dicionario_cnery.inc.php
+++ sistema-novo/includes/dicionario_cnery.inc.php
@@ -9,9 +9,8 @@
         'rodape_endereco_leste' => 'Shopping São José<br>2° Piso em frente a Marisa<br>3342-3327/ 99984-8881',
         'rodape_endereco_nacoes' => 'Parque das Nações<br>R. Angola, n° 21. Quadra 23<br>3654-2283 / 99434-6959',
         'cartao_fone' => '3023-1242<br>3346-0191',
-        'cartao_whatsapp' => '3023-1242<br>3346-0191',
-        'cartao_fone2' => '3023-1242<br>3346-0191',
-        'cartao_whatsapp2' => '3023-1242<br>3346-0191',
+        'cartao_whatsapp' => '99303-9416',
+      
 
 
 
