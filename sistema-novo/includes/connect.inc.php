e23964122d98483e9db6e15138228900e45418d4
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-19 18:10:18 +0200

    Atualizações

--- sistema-novo/includes/connect.inc.php
+++ sistema-novo/includes/connect.inc.php
@@ -6,7 +6,7 @@ include("sessoes.inc.php");
 mysql_connect("ciec-db","root","S3nh@sb@nc0") or die("Erro na conexção ".mysql_error());
 mysql_select_db( "cieceja_".$_SESSION['Dic'] ) or die("Erro no banco ".mysql_error());
 
-
+echo "cieceja_".$_SESSION['Dic'];
 //mysql_connect("ciec-db","cieceja","S3nh@sb@nc0");
 //mysql_select_db( $_SESSION['cook_banco'] );
 
@@ -17,7 +17,6 @@ $conf[resolucao]='Lei Federal n. 9394/96, de 20 de dezembro de 1996.<br>';
 $conf[r]='Lei Federal n. 9394/96, de 20 de dezembro de 1996.';
 
 $conf[unidade] = $Dicionario['unidade'];
-echo $Dicionario['unidade'];
 
 
 list($conf[codigo_curso],$conf[unidade]) = mysql_fetch_row(mysql_query("select codigo, descricao from cadastro_escola where op='1'"));
