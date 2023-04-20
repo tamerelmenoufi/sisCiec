9e1b39cae7cde374c04bec327f42ae87fb2dea6f
livia128924 <livia128924@gmail.com>
2023-04-20 10:02:49 -0300

    atualizando

--- sistema-novo/includes/dicionario_cnery.inc.php
+++ sistema-novo/includes/dicionario_cnery.inc.php
@@ -2,12 +2,16 @@
 
     $Dicionario = [
         'unidade' => 'N_CN_',
-        'resolucao' => 'Lei Federal n. 9394/96, de 20 de dezembro de 1996.<br>',
+        'resolucao' => 'Amparado pela Lei Federal n. 9394/96, de 20 de dezembro de 1996.<br>
+        Curso Reconhecido pela Resolução n° 49/2016 de 30.03.2016 CEE/AM.<br>
+        Exames Autorizados pela Resolução 214/2017, de 20.12.2017 e Resolução 211/2022, de 06.12.2022<br>
+        Manaus/Amazonas',
         'r' => 'Lei Federal n. 9394/96, de 20 de dezembro de 1996.',
         'endereço' => 'Rua fulano de tal',
         'rodape_endereco_djalma' => 'Djalma Batista Alterado<br>Ed. Milhomem Center, Av. Djalma Batista, n° 98A<br>3023-1242 / 3346-0191 / 99303-9416',
         'rodape_endereco_leste' => 'Shopping São José<br>2° Piso em frente a Marisa<br>3342-3327/ 99984-8881',
         'rodape_endereco_nacoes' => 'Parque das Nações<br>R. Angola, n° 21. Quadra 23<br>3654-2283 / 99434-6959',
+        //'ARQUIVO CARTÃO RESPOSTA',
         'cartao_fone' => '3023-1242<br>3346-0191',
         'cartao_whatsapp' => '99303-9416',
         'cartao_local_prova' => 'Djalma Batista, Ed. Milhomem Center, Av. Djalma Batista, No 98A - ao lado do Manaus Plaza Shopping',
@@ -15,6 +19,9 @@
         'cartao_declaracao_taxa' => 'R$ 15,00 - 10 dias úteis',
         'cartao_atestado_taxa' => 'R$ 30,00 - 10 dias úteis',
         'cartao_certificado_taxa' => 'R$ 50,00 - data estabele-cida pela coordenação',
+        //'ARQUIVO ATESTADO DE ELIMICAÇÃO',
+        'atestado_certificado_taxa' => 'R$ 50,00 - data estabele-cida pela coordenação',
+
 
       
 
