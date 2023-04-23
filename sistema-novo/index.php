e240954744143f4d4a978fb78218614fd6523562
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-23 02:24:25 +0200

    Atualizações

--- sistema-novo/index.php
+++ sistema-novo/index.php
@@ -1,9 +1,20 @@
 <?php
+   @session_start();
    if($_GET['desligar']){
       echo "<center><br><br><br><h2>DERVIDOR DESDLIGADO!</h2></center>";
       shell_exec('sudo shutdown');
       file_put_contents('z.txt','dados');
       exit();
    }
+
+   if($_GET['escola']) {
+      $_SESSION['confUnidade'] = $_GET['escola'];
 ?>
-<script>window.location.href='./login/index.php?escola=<?=$_GET['escola']?>';</script>
+      <script>window.location.href='./login/index.php?escola=<?=$_GET['escola']?>';</script>
+<?php
+   }else{
+?>
+      <script>window.location.href='../';</script>
+<?php
+   }
+?>
