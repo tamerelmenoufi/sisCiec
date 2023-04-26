30967f2c8e8bcc040132fbbe1de367e797ea2049
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-26 18:31:30 +0200

    atualizações

--- index.php
+++ index.php
@@ -6,19 +6,19 @@
 		<h1 style="margin-top:200px;">ACESSO AO SISTEMA</h1>
 	<table width='100%' border='0'>
 		<tr>
-			<td width="33%" height="100" align="center" valign="middle">
+			<td width="25%" height="100" align="center" valign="middle">
 				<a href='./sistema-novo/?escola=cnery' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">DJALMA BATISTA</a>
 			</td>
 
-			<td width="33%" height="100" align="center" valign="middle">
+			<td width="25%" height="100" align="center" valign="middle">
 				<a href='./sistema-novo/?escola=leste' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">SÃO JOSÉ</a>
 			</td>
 
-			<td width="33%" height="100" align="center" valign="middle">
+			<td width="25%" height="100" align="center" valign="middle">
 				<a href='./sistema-novo/?escola=sul' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">SUL</a>
 			</td>
 
-			<td width="33%" height="100" align="center" valign="middle">
+			<td width="25%" height="100" align="center" valign="middle">
 				<a href='./sistema-novo/?nacoes' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">PARQUE DAS NAÇÕES</a>
 			</td>
 		</tr>
