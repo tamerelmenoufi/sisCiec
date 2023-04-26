7f04a1729e51613d98cec600c59056c8c6526294
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-26 17:40:04 +0200

    atualizações

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
 	<table width='100%' border='0'>
 		<tr>
 			<td width="33%" height="100" align="center" valign="middle">
-				<a href='./sistema/' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">DJALMA BATISTA</a>
+				<a href='./sistema-novo/?escola=cnery' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">DJALMA BATISTA</a>
 			</td>
 
 			<td width="33%" height="100" align="center" valign="middle">
-				<a href='./sistema-novo/?escola=cnery' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">SISTEMA NOVA VERSÃO</a>
+				<a href='./sistema-novo/?escola=leste' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">SÃO JOSÉ</a>
 			</td>
 
 			<td width="33%" height="100" align="center" valign="middle">
-				<a href='./nacoes/' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">PARQUE DAS NAÇÕES</a>
+				<a href='./sistema-novo/?nacoes' style="padding:20px; background:blue; color:#fff; border-radius:20px; text-decoration:none;">PARQUE DAS NAÇÕES</a>
 			</td>
 		</tr>
 
