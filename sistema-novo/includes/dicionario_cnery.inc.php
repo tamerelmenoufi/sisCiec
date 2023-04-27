a8a3bd95b240cd0d56f96eb5472a8fa169f9ada9
livia128924 <livia128924@gmail.com>
2023-04-27 09:37:14 -0300

    atualizando

--- sistema-novo/includes/dicionario_cnery.inc.php
+++ sistema-novo/includes/dicionario_cnery.inc.php
@@ -14,13 +14,13 @@
         //'ARQUIVO CARTÃO RESPOSTA',
         'cartao_fone' => '3023-1242<br>3346-0191',
         'cartao_whatsapp' => '99303-9416',
-        'cartao_local_prova' => 'Djalma Batista, Ed. Milhomem Center, Av. Djalma Batista, No 98A - ao lado do Manaus Plaza Shopping',
+        'cartao_local_prova' => 'Djalma Batista, Ed. Milhomem Center, Av. Djalma Batista, N° 98A - ao lado do Manaus Plaza Shopping',
         'cartao_obs_prova' => 'Resultado Oficial do Exame Somente 10 dias ap&oacute;s a prova',
         'cartao_declaracao_taxa' => 'R$ 15,00 - 10 dias úteis',
         'cartao_atestado_taxa' => 'R$ 30,00 - 10 dias úteis',
         'cartao_certificado_taxa' => 'R$ 50,00 - data estabele-cida pela coordenação',
         //'ARQUIVO CERTIFICADO',
-        'certificado_local' => 'Av. Djalma Batista, n°98 A, salas 200/201/202/218, 2° andar, Parque Dez de Novembro. CEP 69055-038 Manaus/AM',
+        'certificado_local' => 'Av. Djalma Batista, n° 98A, salas 200/201/202/218, 2° andar, Parque Dez de Novembro. CEP 69055-038 Manaus/AM',
         'certificado_resolucoes' => 'Resolução de Autorização para Exames de EJA emanadas pelo Conselho Estadual de Educação do Estado do Amazonas - CEE/AM : Resolução n° 38/05, de 26.04.2005/ Resolução 96/06 de 19.09.2006/ Resolução 71/07, de 02.07.2007/ Resolução 92/07, de 08.08.2007/ Resolução 145/07, de 11.12.2007/ Resolução 179/2011, de 20.12.2011/ Resolução 213/2013 de 20.09.2013/ Resolução 49/2016, de 30.03.2016/ Resolução 159/2016, de 16.11.2016/ Resolução 245/2015, de 17.12.2015/ Resolução 214/2017, de 20.12.2017/ Resolução 211/2022, de 06.12.2022.',
       
 
