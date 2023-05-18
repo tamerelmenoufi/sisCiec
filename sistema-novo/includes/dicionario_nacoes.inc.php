923908b20e7f3d2760740a000c5c67e4c7049210
livia128924 <livia128924@gmail.com>
2023-05-18 15:31:55 -0300

    atualizando

--- sistema-novo/includes/dicionario_nacoes.inc.php
+++ sistema-novo/includes/dicionario_nacoes.inc.php
@@ -9,7 +9,7 @@
         'r' => 'Lei Federal n. 9394/96, de 20 de dezembro de 1996.',
         'endereço' => 'Rua fulano de tal',
         'rodape_endereco_djalma' => 'Djalma Batista<br>Ed. Milhomem Center, Av. Djalma Batista, n° 98A<br>3023-1242 / 3346-0191 / 99303-9416',
-        'rodape_endereco_leste' => 'Shopping São José<br> Piso da Praça de alimentação<br>3342-3327/ 99984-8881',
+        'rodape_endereco_leste' => 'Shopping São José<br> 1° piso<br>3342-3327/ 99984-8881',
         'rodape_endereco_nacoes' => 'Parque das Nações<br>R. Angola, n° 21. Quadra 23<br>3654-2283 / 99434-6959',
         //'ARQUIVO CARTÃO RESPOSTA',
         'cartao_fone' => '3023-1242<br>3346-0191',
